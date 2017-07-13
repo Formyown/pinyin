db097f5f281d3cd4ecbeb465dc6b7500c6e55bba
11192 <1119242459@qq.com>
2017-07-14 04:44:41 +1000

    init

---
+++ PinyinToneMarker/PinyinModel.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace PinyinToneMarker
+{
+    public class Pinyin
+    {
+        private char[] contentArray;
+        private string _content;
+        private string fullFormat = null;
+        public static string MarkTone(string content, int tone)
+        {
+            char[] contentArray = content.ToLower().ToCharArray();
+            GetFullFormat(contentArray, tone);
+            return new String(contentArray);
+        }
+
+        private static readonly char[,] Vowels = new char[,]
+        {
+            { 'a', 'e', 'i', 'o', 'u', 'ü', 'm', 'n'},
+            { 'ā', 'ē', 'ī', 'ō', 'ū', 'ǖ', 'm', 'n'},
+            { 'á', 'é', 'í', 'ó', 'ú', 'ǘ', 'ḿ', 'ń'},
+            { 'ǎ', 'ě', 'ǐ', 'ǒ', 'ǔ', 'ǚ', 'm', 'ň'},
+            { 'à', 'è', 'ì', 'ò', 'ù', 'ǜ', 'm', 'ǹ'}
+        };
+        private enum VowelsIndex : int
+        {
+            A = 0, E, I, O, U, V
+        }
+        private enum SyllablesIndex : int
+        {
+           M = VowelsIndex.V + 1, N, J , Q , X
+        }
+        
+        private static void GetFullFormat(char[] content, int tone)
+        {
+            if(tone < 1 || tone > 4) return;
+
+            bool hasJQXY = false;
+           
+            int[] indexes = new int[15];
+            for(int i = 0; i < indexes.Length; i++)
+            {
+                indexes[i] = -1;
+            }
+            for(int i = 0;i < content.Length; i++)
+            {
+                switch(content[i])
+                {
+                    case 'a': indexes[(int)VowelsIndex.A] = i; break;
+                    case 'o': indexes[(int)VowelsIndex.O] = i; break;
+                    case 'e': indexes[(int)VowelsIndex.E] = i; break;
+                    case 'i': indexes[(int)VowelsIndex.I] = i; break;
+                    case 'u': indexes[(int)VowelsIndex.U] = i; break;
+                    case 'v': indexes[(int)VowelsIndex.V] = i; break;
+
+                    case 'n': indexes[(int)SyllablesIndex.M] = i; break;
+                    case 'm': indexes[(int)SyllablesIndex.N] = i; break;
+
+                    case 'j': 
+                    case 'q': 
+                    case 'x':
+                    case 'y': hasJQXY = true; break;
+                }
+            }
+            int index = indexes[(int)VowelsIndex.V];
+            if (index >= 0)
+            {
+                content[index] = hasJQXY ? 'u': 'ü';
+            }
+
+
+            if ((index = indexes[(int)VowelsIndex.A]) >= 0)
+            { content[index] = Vowels[tone, (int)VowelsIndex.A]; return; }
+
+            if ((index = indexes[(int)VowelsIndex.O]) >= 0)
+            { content[index] = Vowels[tone, (int)VowelsIndex.O]; return; }
+
+            if ((index = indexes[(int)VowelsIndex.E]) >= 0)
+            { content[index] = Vowels[tone, (int)VowelsIndex.E]; return; }
+
+            index = indexes[(int)VowelsIndex.I];
+
+            int index2;
+            if ((index2 = indexes[(int)VowelsIndex.U]) > index)
+            { content[index2] = Vowels[tone, (int)VowelsIndex.U]; return; }
+
+            if ((index2 = indexes[(int)VowelsIndex.V]) > index)
+            { content[index2] = Vowels[tone, hasJQXY ? (int)VowelsIndex.U : (int)VowelsIndex.V]; return; }
+
+            if (index >= 0)
+            { content[index] = Vowels[tone, (int)VowelsIndex.I]; return; }
+            
+        }
+    }
+}
---
+++ PinyinToneMarker/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PinyinToneMarker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("输入拼音和声调(多组以空格分割,轻声不标示),Ctrl+C退出");
+            Console.Out.WriteLine("例如: > han4 yu3 pin1 yin1");
+
+            while (true)
+            {
+                Console.Out.Write("> ");
+                string[] items = Console.ReadLine().Split(new char[] { ' '});
+                for(int i = 0; i < items.Length; ++i)
+                {
+                    string content = items[i].Trim();
+                    if (string.IsNullOrEmpty(content)) continue;
+                    string result;
+                    if (char.IsDigit(content[content.Length - 1]))
+                    {
+                        result = Pinyin.MarkTone(content.Substring(0, content.Length - 1), int.Parse(content[content.Length - 1].ToString()));
+                    }
+                    else
+                    {
+                        result = Pinyin.MarkTone(content, 0);
+                    }
+                    Console.Out.WriteLine("{0}.{1}", i, result);
+                }
+
+            }
+           
+        }
+    }
+}
